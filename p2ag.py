32032d4abe2c74990301ea35a70b8625ffd2c2c5
gloomy <dkfptm335@naver.com>
2024-04-12 15:02:19 +0900

    Add main page and signup logics

--- p2ag.py
+++ p2ag.py
@@ -1,8 +1,37 @@
 import pandas as pd
 import requests
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
+
+token = r'secret_DMk5aGpHhepTCDPLnpslUaxNFIL5Az2sAGi23PI7rdR'
+database_id = r'031db1df89214a38bbf9302803d5c5b6'
+headers = {
+    "Authorization": "Bearer " + token,
+    "Content-Type": "application/json",
+    "Notion-Version": "2022-06-28"
+}
+
+url = f'https://api.notion.com/v1/databases/{database_id}/query'
+
+data = requests.post(url, headers=headers).json()
+
+id = []
+pw = []
+name = []
+email = []
+
+for result in data['results']:
+    if result['properties']['id']['title']:
+        id.append(result['properties']['id']['title'][0]['plain_text'])
+    if result['properties']['pw']['rich_text']:
+        pw.append(result['properties']['pw']['rich_text'][0]['plain_text'])
+    if result['properties']['name']['rich_text']:
+        name.append(result['properties']['name']['rich_text'][0]['plain_text'])
+    if result['properties']['email']['rich_text']:
+        email.append(result['properties']['email']['rich_text'][0]['plain_text'])
+
+df = pd.DataFrame(zip(id, pw, name, email), columns=['id', 'pw', 'name', 'email'])
 
 
 @app.route('/')
@@ -15,35 +44,6 @@ def main():
     userId = request.form['userId']
     userPw = request.form['userPw']
 
-    token = r'secret_DMk5aGpHhepTCDPLnpslUaxNFIL5Az2sAGi23PI7rdR'
-    database_id = r'031db1df89214a38bbf9302803d5c5b6'
-    headers = {
-        "Authorization": "Bearer " + token,
-        "Content-Type": "application/json",
-        "Notion-Version": "2022-06-28"
-    }
-
-    url = f'https://api.notion.com/v1/databases/{database_id}/query'
-
-    data = requests.post(url, headers=headers).json()
-
-    id = []
-    pw = []
-    name = []
-    gender = []
-
-    for result in data['results']:
-        if result['properties']['id']['title']:
-            id.append(result['properties']['id']['title'][0]['plain_text'])
-        if result['properties']['pw']['rich_text']:
-            pw.append(result['properties']['pw']['rich_text'][0]['plain_text'])
-        if result['properties']['name']['rich_text']:
-            name.append(result['properties']['name']['rich_text'][0]['plain_text'])
-        if result['properties']['gender']['rich_text']:
-            gender.append(result['properties']['gender']['rich_text'][0]['plain_text'])
-
-    df = pd.DataFrame(zip(id, pw, name, gender), columns=['id', 'pw', 'name', 'gender'])
-
     if userId in df['id'].values:
         if userPw == df[df['id'] == userId]['pw'].values[0]:
             return render_template('main.html', userId=userId, userPw=userPw,
@@ -53,8 +53,29 @@ def main():
 
 
 @app.route('/signUp', methods=['POST'])
-def signup():
-    return render_template('signUp.html')
+def signUp():
+    userNewId = request.form['userNewId']
+    userNewPw = request.form['userNewPw']
+    userNewPw2 = request.form['userNewPw2']
+    userNewName = request.form['userNewName']
+    userNewEmail = request.form['userNewEmail']
+
+    if userNewId in df['id'].values:
+        return render_template('duplicatedId.html')
+    elif userNewEmail in df['email'].values:
+        return render_template('duplicatedEmail.html')
+    elif userNewPw != userNewPw2:
+        return render_template('notMatch.html')
+    else:
+        return render_template('signUp.html', userNewId=userNewId, userNewPw=userNewPw,
+                               userNewName=userNewName, userNewEmail=userNewEmail)
+
+
+@app.route('/submitSelection', methods=['POST'])
+def submitSelection():
+    selection = request.form['selection']
+    if selection == 'university':
+        return redirect(url_for('univForm'))
 
 
 @app.route('/univForm')
