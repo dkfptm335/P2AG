0e8d6e322f6955dff05f840656bc0b3a946f1b69
gloomy <dkfptm335@naver.com>
2024-04-12 11:45:51 +0900

    Add login check

--- p2ag.py
+++ p2ag.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import requests
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -7,20 +9,55 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+
 @app.route('/main', methods=['POST'])
 def main():
-    id_ = request.form['id_']
-    pw_ = request.form['pw_']
-    print(id_, pw_)
-    return render_template('main.html')
+    userId = request.form['userId']
+    userPw = request.form['userPw']
 
-@app.route('/univform')
-def univform():
+    token = r'secret_DMk5aGpHhepTCDPLnpslUaxNFIL5Az2sAGi23PI7rdR'
+    database_id = r'031db1df89214a38bbf9302803d5c5b6'
+    headers = {
+        "Authorization": "Bearer " + token,
+        "Content-Type": "application/json",
+        "Notion-Version": "2022-06-28"
+    }
 
-    return render_template('univform.html')
+    url = f'https://api.notion.com/v1/databases/{database_id}/query'
 
-@app.route('/submit', methods=['POST'])
-def submit():
+    data = requests.post(url, headers=headers).json()
+
+    id = []
+    pw = []
+    name = []
+    gender = []
+
+    for result in data['results']:
+        if result['properties']['id']['title']:
+            id.append(result['properties']['id']['title'][0]['plain_text'])
+        if result['properties']['pw']['rich_text']:
+            pw.append(result['properties']['pw']['rich_text'][0]['plain_text'])
+        if result['properties']['name']['rich_text']:
+            name.append(result['properties']['name']['rich_text'][0]['plain_text'])
+        if result['properties']['gender']['rich_text']:
+            gender.append(result['properties']['gender']['rich_text'][0]['plain_text'])
+
+    df = pd.DataFrame(zip(id, pw, name, gender), columns=['id', 'pw', 'name', 'gender'])
+
+    if userId in df['id'].values:
+        if userPw == df[df['id'] == userId]['pw'].values[0]:
+            return render_template('main.html', userId=userId, userPw=userPw, userName=df[df['id'] == userId]['name'].values[0], userGender=df[df['id'] == userId])
+        else:
+            return "비밀번호가 일치하지 않습니다."
+
+
+@app.route('/univForm')
+def univForm():
+    return render_template('univForm.html')
+
+
+@app.route('/univResult', methods=['POST'])
+def univResult():
     provider = request.form.get('provider')
     date = request.form.get('date')
     policy = request.form.get('policy')
