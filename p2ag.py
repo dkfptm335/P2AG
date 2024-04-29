3afc6111f5026eada489759dd6707c99e602e677
gloomy <dkfptm335@naver.com>
2024-04-29 12:49:09 +0900

    세션 정보 로컬에 저장하는 것으로 변경

--- p2ag.py
+++ p2ag.py
@@ -1,11 +1,19 @@
+import re
+
 import pandas as pd
+import requests
+from bs4 import BeautifulSoup as bs
 from flask import Flask, request, session
 from flask import render_template, redirect, url_for
-import requests, re
-from bs4 import BeautifulSoup as bs
+from flask_session import Session
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # 세션을 위한 시크릿 키 설정
+app.config['SESSION_TYPE'] = 'filesystem'
+
+sess = Session()
+sess.init_app(app)
+
 
 def extract_and_verify(url):
     headers = {'User-Agent': 'Mozilla/5.0'}
@@ -15,7 +23,7 @@ def extract_and_verify(url):
     text = soup.get_text()
     text = text.replace(' ', '').replace('\n', '').replace('\r', '').replace('\t', '')
 
- # 각 검사 함수를 호출
+    # 각 검사 함수를 호출
     item_results = check_required_items(text)
     keyword_results = check_keywords(text)
     description_results = check_descriptions(text)
@@ -29,6 +37,7 @@ def extract_and_verify(url):
         '표현방식 검사 결과': compliance_results
     }
     return categories
+
 
 def check_required_items(text):
     missing_items = []
@@ -48,19 +57,20 @@ def check_required_items(text):
 
     return "누락된 필수 항목: " + ", ".join(missing_items) if missing_items else "모든 필수 항목이 존재합니다."
 
+
 def check_keywords(text):
     missing_keywords = []
     keywords = [
-    ('개인정보파일명', '파일명', '개인정보파일', '개인정보파일의명칭'),
-    ('수집목적', '개인정보파일의운영목적', '운영목적', '개인정보수집/이용목적', '처리목적'),
-    ('수집항목', '처리하는개인정보의항목', '처리하는개인정보항목', '개인정보의항목'),
-    ('보유근거', '처리근거', '운영근거'),
-    ('보유및이용기간','개인정보보유기간', '제공기간', '처리기간', '처리및보유기간','보유및처리기간'),
-    ('제공받는자', '제공기관', '제공받는기관','제공현황','제공목록', '제공리스트'),
-    ('제공목적', '제공받는자의이용목적', '이용목적'),
-    ('제공정보', '제공하는항목', '제공하는개인정보항목', '제공항목', '개인정보파일명','제공현황', '제공리스트'),
-    ('수탁자', '업체명', '위탁대상', '위탁업체명', '수탁기관', '수탁업체명', '수탁사', '수탁업체', '수탁업체명칭'),
-    ('위탁하는업무내용', '업무목적', '위탁업무내용', '위탁업무', '위탁항목', '업무내용', '위탁하는업무의내용')
+        ('개인정보파일명', '파일명', '개인정보파일', '개인정보파일의명칭'),
+        ('수집목적', '개인정보파일의운영목적', '운영목적', '개인정보수집/이용목적', '처리목적'),
+        ('수집항목', '처리하는개인정보의항목', '처리하는개인정보항목', '개인정보의항목'),
+        ('보유근거', '처리근거', '운영근거'),
+        ('보유및이용기간', '개인정보보유기간', '제공기간', '처리기간', '처리및보유기간', '보유및처리기간'),
+        ('제공받는자', '제공기관', '제공받는기관', '제공현황', '제공목록', '제공리스트'),
+        ('제공목적', '제공받는자의이용목적', '이용목적'),
+        ('제공정보', '제공하는항목', '제공하는개인정보항목', '제공항목', '개인정보파일명', '제공현황', '제공리스트'),
+        ('수탁자', '업체명', '위탁대상', '위탁업체명', '수탁기관', '수탁업체명', '수탁사', '수탁업체', '수탁업체명칭'),
+        ('위탁하는업무내용', '업무목적', '위탁업무내용', '위탁업무', '위탁항목', '업무내용', '위탁하는업무의내용')
     ]
 
     for group in keywords:
@@ -69,15 +79,17 @@ def check_keywords(text):
 
     return "누락된 키워드: " + ", ".join(missing_keywords) if missing_keywords else "모든 키워드가 존재합니다."
 
+
 def check_descriptions(text):
     issues = []
     ambiguous_terms = ["기타등등", "등", "기타", "정부기관", "공공기관", "관련기관", "협회",
-                       "보험회사", "목적달성시", "타법령에따라", "수시", "상시", "사유발생시","관계법률에"]
+                       "보험회사", "목적달성시", "타법령에따라", "수시", "상시", "사유발생시", "관계법률에"]
     for term in ambiguous_terms:
         if term in text:
             issues.append(f"모호한 용어 '{term}'이(가) 사용됨.")
 
     return "\n".join(issues) if issues else "문서에 모호한 용어가 없습니다."
+
 
 def check_compliance(text):
     results = []
@@ -89,6 +101,7 @@ def check_compliance(text):
         results.append("이메일 형식이 잘못되었습니다. 올바른 형식 예: example@example.com")
 
     return "\n".join(results) if results else "모든 표현이 적절합니다."
+
 
 @app.route('/')
 def index():
@@ -514,7 +527,8 @@ def nextForm1_3Confirm():
 
     if request.form['action'] == 'confirm':
         return render_template('nextForm1_3Confirm.html', name=name, checkbox2=checkbox2, checkbox1=checkbox1,
-                               auto_collect=auto_collect, table1=table1, table2=table2, table3=table3, sub_check1=sub_check1, sub_check2=sub_check2)
+                               auto_collect=auto_collect, table1=table1, table2=table2, table3=table3,
+                               sub_check1=sub_check1, sub_check2=sub_check2)
     else:
         return redirect(url_for('nextForm2'))
 
@@ -688,6 +702,10 @@ def result():
     form_data2 = session.get('form_data_2', {})
     form_data3 = session.get('form_data_3', {})
     form_data4 = session.get('form_data_4', {})
+    print(form_data1)
+    print(form_data2)
+    print(form_data3)
+    print(form_data4)
 
     return render_template('result.html', form_data1=form_data1, form_data2=form_data2, form_data4=form_data4,
                            academic_df=academic_df, scholarship_df=scholarship_df, grade_df=grade_df,
