ac866a20385de61b75628e3b1b75541b8185e5d0
Hyeon <gpdus0321@naver.com>
2024-04-29 13:16:44 +0900

    완성

--- p2ag.py
+++ p2ag.py
@@ -129,7 +129,6 @@ def index():
     homepage_df = pd.DataFrame()
     etc_df = pd.DataFrame()
     combined_data_result = dict()
-    date_ranges = []
     selected_rows = []
     manager_position = ''
     manager_affiliation = ''
@@ -180,7 +179,6 @@ disorder_df = pd.DataFrame()
 homepage_df = pd.DataFrame()
 etc_df = pd.DataFrame()
 combined_data_result = dict()
-date_ranges = []
 
 
 def process_form_data1(request):
@@ -193,45 +191,56 @@ def process_form_data1(request):
     global homepage_df
     global etc_df
     global combined_data_result
-    global date_ranges
+    global destructionProcedure
+    global rightsAndObligations
+    global remedyMethod
 
     name = request.form['name']
     file = request.files['file']
+
+    # 개인정보의 파기 절차 및 방법
+    destructionProcedure = request.form['destructionProcedure']
+
+    # 정보주체와 법정대리인의 권리/의무 및 행사방법
+    rightsAndObligations = request.form['rightsAndObligations']
+
+    # 정보주체의 권익침해에 대한 구제방법
+    remedyMethod = request.form['remedyMethod']
 
     # 체크박스 확인
     selected_datas = request.form.getlist('securityMeasures')
     result_head = []
     result_messages = []
     if 'minimalStaffTraining' in selected_datas:
-        result_head.append('[관리]개인정보 취급 직원의 최소화 및 교육')
+        result_head.append('개인정보 취급 직원의 최소화 및 교육')
         result_messages.append('개인정보를 취급하는 담당자는 반드시 필요한 인원에 한하여 지정ㆍ관리하고 있으며, 취급자를 대상으로 안전한 관리를 위한 교육을 실시하고 있습니다.')
     if 'regularSelfAudit' in selected_datas:
-        result_head.append('[관리]정기적인 자체 감사')
+        result_head.append('정기적인 자체 감사')
         result_messages.append('개인정보 취급 관련 안정성 확보를 위해 정기적으로 자체 감사를 실시하고 있습니다.')
     if 'internalManagementPlan' in selected_datas:
-        result_head.append('[관리]내부관리계획 수립 및 시행')
+        result_head.append('내부관리계획 수립 및 시행')
         result_messages.append('개인정보의 안전한 처리를 위하여 내부관리계획을 수립․시행하고 있습니다.')
     if 'dataEncryption' in selected_datas:
-        result_head.append('[기술]개인정보의 암호화')
+        result_head.append('개인정보의 암호화')
         result_messages.append(
             '이용자의 개인정보 중 비밀번호, 주민등록번호는 암호화 되어 저장 및 관리되고 있어, 본인만이 알 수 있으며 중요한 데이터는 파일 및 전송 데이터를 암호화 하거나 파일 잠금 기능을 사용하는 등의 별도 보안기능을 사용하고 있습니다.')
     if 'securitySoftware' in selected_datas:
-        result_head.append('[기술]보안프로그램 설치 및 주기적 점검·갱신')
+        result_head.append('보안프로그램 설치 및 주기적 점검·갱신')
         result_messages.append(
             '해킹이나 컴퓨터 바이러스 등에 의한 개인정보 유출 및 훼손을 막기 위하여 보안프로그램을 설치하고 주기적인 갱신·점검을 하며 외부로부터 접근이 통제된 구역에 시스템을 설치하고 기술적/물리적으로 감시 및 차단하고 있습니다.')
     if 'accessRestriction' in selected_datas:
-        result_head.append('[기술]개인정보에 대한 접근 제한')
+        result_head.append('개인정보에 대한 접근 제한')
         result_messages.append(
             '개인정보를 처리하는 데이터베이스시스템에 대한 접근권한의 부여,변경,말소를 통하여 개인정보에 대한 접근통제를 위하여 필요한 조치를 하고 있으며 침입차단시스템을 이용하여 외부로부터의 무단 접근을 통제하고 있습니다.')
     if 'unauthorizedAccessControl' in selected_datas:
-        result_head.append('[물리]비인가자에 대한 출입 통제')
+        result_head.append('비인가자에 대한 출입 통제')
         result_messages.append('개인정보를 보관하고 있는 물리적 보관 장소를 별도로 두고 이에 대해 출입통제 절차를 수립, 운영하고 있습니다.')
     if 'logManagement' in selected_datas:
-        result_head.append('[관리]접속기록의 보관 및 위 변조 방지')
+        result_head.append('접속기록의 보관 및 위 변조 방지')
         result_messages.append(
             '개인정보처리시스템에 접속한 기록을 최소 1년 이상 보관, 관리하고 있으며, 접속기록이 위·변조 및 도난, 분실되지 않도록 월 1회 이상 점검하고 있습니다. 다만, 5만 명 이상의 정보주체에 관하여 개인정보를 처리하거나, 고유식별정보 또는 민감정보를 처리하는 개인정보처리시스템의 경우에는 2년 이상 보관 및 관리하고 있습니다.')
     if 'documentSecurity' in selected_datas:
-        result_head.append('[물리]문서보안을 위한 잠금장치 사용')
+        result_head.append('문서보안을 위한 잠금장치 사용')
         result_messages.append('개인정보가 포함된 서류, 보조저장매체 등을 잠금 장치가 있는 안전한 장소에 보관하고 있습니다.')
 
     if not result_messages:
@@ -260,11 +269,6 @@ def process_form_data1(request):
 
     # 현재 날짜 받아오기
     current_date = request.form['current_date']
-
-    # 제출된 데이터를 가져온다
-    start_dates = request.form.getlist('start_date')
-    end_dates = request.form.getlist('end_date')
-    date_ranges = [{'start_date': start, 'end_date': end} for start, end in zip(start_dates, end_dates)]
 
     # header=0 옵션으로 첫 번째 행을 칼럼명으로 사용
     df = pd.read_excel(file, header=None)
@@ -324,11 +328,13 @@ def process_form_data1(request):
         'scholarship_df': scholarship_df,
         'grade_df': grade_df,
         'graduate_df': graduate_df,
-        'date_ranges': date_ranges,
         'newStudent_df': newStudent_df,
         'disorder_df': disorder_df,
         'homepage_df': homepage_df,
-        'etc_df': etc_df
+        'etc_df': etc_df,
+        'destructionProcedure': destructionProcedure,
+        'rightsAndObligations': rightsAndObligations,
+        'remedyMethod': remedyMethod
     }
 
 
@@ -500,6 +506,8 @@ def nextForm1_3Confirm():
     table3_2 = request.form.getlist('table3_2')
     table3 = zip(table3_1, table3_2)
 
+    safety_measure = request.form['safety_measure']
+
     try:
         sub_check1 = request.form['sub_check1']
     except:
@@ -514,7 +522,7 @@ def nextForm1_3Confirm():
 
     if request.form['action'] == 'confirm':
         return render_template('nextForm1_3Confirm.html', name=name, checkbox2=checkbox2, checkbox1=checkbox1,
-                               auto_collect=auto_collect, table1=table1, table2=table2, table3=table3, sub_check1=sub_check1, sub_check2=sub_check2)
+                               auto_collect=auto_collect, safety_measure=safety_measure, table1=table1, table2=table2, table3=table3, sub_check1=sub_check1, sub_check2=sub_check2)
     else:
         return redirect(url_for('nextForm2'))
 
@@ -591,17 +599,12 @@ def process_form_data3(request):
     checking_method = request.form['checking_method']
     checking_location = request.form['checking_location']
 
-    # 6~7
-    checkListYes = request.form.getlist('checkListYes')
-    if 'sub7' in checkListYes:
-        checkList7.append('‣  귀하는 개인영상정보에 관하여 열람 또는 존재확인ㆍ삭제를 원하는 경우 언제든지 영상정보처리기기 운영자에게 요구하실 수 있습니다. ')
-        checkList7.append('‣  단, 귀하가 촬영된 개인영상정보 및 명백히 정보주체의 급박한 생명, 신체, 재산의 이익을 위하여 필요한 개인영상정보에 한정됩니다.')
-        checkList7.append('‣  본 기관은 개인영상정보에 관하여 열람 또는 존재확인ㆍ삭제를 요구한 경우 지체없이 필요한 조치를 하겠습니다.')
-    if 'sub8' in checkListYes:
-        checkList8.append('‣  본 기관에서 처리하는 영상정보는 암호화 조치 등을 통하여 안전하게 관리되고 있습니다. ')
-        checkList8.append(
-            '‣  또한 본 기관은 개인영상정보보호를 위한 관리적 대책으로서 개인정보에 대한 접근 권한을 차등부여하고 있고, 개인영상정보의 위ㆍ변조 방지를 위하여 개인영상정보의 생성 일시, 열람시 열람 목적ㆍ열람자ㆍ열람 일시 등을 기록하여 관리하고 있습니다. ')
-        checkList8.append('‣  이 외에도 개인영상정보의 안전한 물리적 보관을 위하여 잠금장치를 설치하고 있습니다')
+    # 6. 정보주체의 영상정보 열람 등 요구에 대한 조치
+    requestView = request.form['requestView']
+
+
+    #7. 영상정보 보호를 위한 안정성 확보 조치
+    stability = request.form['stability']
 
     # 8. 영상정보처리기기 운영·관리 방침의 변경에 관한 사항
     # 현재 날짜 받아오기
@@ -611,6 +614,17 @@ def process_form_data3(request):
     checkBox2 = ''
     if 'checkBox2' in request.form.getlist('checkBox2'):
         checkBox2 = 1
+    fourteen = 0
+    try:
+        radio14 = request.form['radio14']
+        print(radio14)
+        if radio14 == 'yes':
+            fourteen = 1
+            print(fourteen)
+        else:
+            fourteen = 0
+    except:
+        fourteen = 0
 
     # 처리된 데이터를 딕셔너리로 반환
     return {
@@ -634,11 +648,13 @@ def process_form_data3(request):
         'trustee_tel': trustee_tel,
         'checkList7': checkList7,
         'checkList8': checkList8,
-        'checkBox4': checkBox4,
         'checking_method': checking_method,
         'checking_location': checking_location,
         'current_date': current_date,
-        'checkBox2': checkBox2
+        'checkBox2': checkBox2,
+        'requestView': requestView,
+        'stability': stability,
+        'fourteen': fourteen
     }
 
 
